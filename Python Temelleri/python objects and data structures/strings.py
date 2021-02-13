66e9d71fc2622e7c9bb31f48622be52c20b593d9
Ayşe <Ayseekinci.02.00@gmail.com>
2021-02-13 22:26:04 +0300

    Create strings.py

---
+++ Python Temelleri/python objects and data structures/strings.py
@@ -0,0 +1,16 @@
+# kendini tanıt
+# 4. karekteri yazdır
+# 3. karekter ile 18. karekter arasındakileri yazdır
+# toplam karekter sayısını bul yazdır.
+
+name = "Ayşe"
+surName = "Ekinci"
+age = 23
+
+tanim = 'Benim adım ' + name + ' soyadım ' + \
+    surName + str(age) + ' yaşındayım. '
+
+print(tanim)
+print(tanim[4])
+print(tanim[3:18])
+print(len(tanim))

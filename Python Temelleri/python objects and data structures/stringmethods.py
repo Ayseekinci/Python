3d2e311f07dd0c81245050cd04120da99ee1fcaf
Ayşe <Ayseekinci.02.00@gmail.com>
2021-02-15 00:19:21 +0300

    Create stringmethods.py

---
+++ Python Temelleri/python objects and data structures/stringmethods.py
@@ -0,0 +1,53 @@
+message = "Hello there. My name is Ayşe. "
+
+
+"""message=message.upper()       #hepsini büyük harf olarak yazar.
+print(message)
+
+message=message.title()       #tüm baş harfleri büyük yazar.
+print(message)
+
+message=message.lower()       #hepsini küçük harf olarak yazar.
+print(message)
+
+message=message.capitalize()  # sadece ilk kelimenin baş harfini büyük yazar gerisini küçük yazar.
+print(message)
+
+message=message.strip()       #Eğer başta boşluk varsa siler.
+print(message)
+
+message=message.split()       #cümleyi parçalara ayırır.
+print(message)
+
+message=message.split('.')    #cümleyi nokta ile birlikte 2 ye böler.
+print(message[1])                #böldükten sonra 2.cümleyi ekrana yazar.
+
+message= ' '.join(message)      #cümleyi tekrar birleştirir.
+print(message)
+
+message='*'.join(message)    #Aralara yıldız ekler.
+print(message)
+
+index=message.find('Ayşe')      #Ayşeyi arar ilk gelen indeksin numarasını yazar.
+print(index)
+
+baslar=message.startswith('H')  #cümle 'H'ile başlayıp başlamadığını yazar.
+print(baslar)
+
+biter=message.endswith(' t')    #cümle 't' ile bitip bitmediğini yazar.
+print(biter)
+
+message=message.replace('Ayşe','Fatma')  #cümle içerisinde değişiklik yapar.
+print(message)
+
+message=message.center(50,'*')   #50 karakterlik yer ayırır ve boşta kalan yerlere '*' koyar.
+print(message)
+
+result=message.count('e',0,10)  # 0 ile 10 indexleri arasında kaç tane e harfi var
+print(result)
+
+result=message.isalpha()        #cümle alfametik sıraya göre mi sıralanmış ? 
+print(result)
+
+result=message.isdigit()         # gelen degerler rakam mı ?
+print(result)"""

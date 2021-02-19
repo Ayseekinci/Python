f892372a66cd22dbe4cdab44c7959172928d6de8
Ayşe <Ayseekinci.02.00@gmail.com>
2021-02-19 20:44:12 +0300

    Create main.py

---
+++ Python Temelleri/python modules/main.py
@@ -0,0 +1,15 @@
+import mod
+
+"""result = help(mod)  # mod klasçrü hakkında bilgi verir
+
+result = help(mod.fonk)  # mod klasöründeki fonksiyon hakında bilgi verir.
+result = mod.numbers  # mod klasöründeki numbers listesini yazdırır.
+print(result)
+"""
+
+
+"""p = mod.Person()  # mod klasöründeki personel classörünü çağır
+p.tanisma()  # personel classın içerisindeki tanışma fonksiyonun yazıdır.
+
+print(p)
+"""

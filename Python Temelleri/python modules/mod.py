733588af29d4a741200cff181e925314e1364cc9
Ayşe <Ayseekinci.02.00@gmail.com>
2021-02-19 20:43:20 +0300

    Create mod.py

---
+++ Python Temelleri/python modules/mod.py
@@ -0,0 +1,18 @@
+number = 15
+numbers = [2, 3, 8, 4, 9]
+
+person = {
+    'Name': 'Ayşe',
+    'Year': 1998,
+    'City': 'Adıyaman'
+
+}
+
+
+def fonk(x):  # dışarıdan bir x parametresi alır
+    print(f"x : {x}")
+
+
+class Person:  # person adında bir class oluşruldu
+    def tanisma(self):
+        print("My name is AYŞE..")

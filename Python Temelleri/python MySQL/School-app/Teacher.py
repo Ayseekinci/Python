f61e216c7f690fbce3a5aa354b78747754e9d67a
Ayşe <Ayseekinci.02.00@gmail.com>
2021-03-17 11:35:00 +0300

    Create Teacher.py

---
+++ Python Temelleri/python MySQL/School-app/Teacher.py
@@ -0,0 +1,8 @@
+class Teacher:
+    def __init__(self, id, branch, name, surname, birthdate, gender, classid):
+        self.id = id
+        self.branch = branch
+        self.name = name
+        self.surname = surname
+        self.birtdate = birthdate
+        self.gender = gender

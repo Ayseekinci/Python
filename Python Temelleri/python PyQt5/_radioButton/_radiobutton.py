39eb95f8ea6d336ead174c2ee23a5cda506893f8
Ayşe <Ayseekinci.02.00@gmail.com>
2021-03-20 00:06:05 +0300

    Create _radiobutton.py

---
+++ Python Temelleri/python PyQt5/_radioButton/_radiobutton.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5 import QtWidgets
+from _radiobuttonForm import Ui_MainWindow
+
+
+class myApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(myApp, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.radioButton.toggled.connect(self.onChecked)
+        self.ui.radioButton_2.toggled.connect(self.onChecked)
+        self.ui.radioButton_3.toggled.connect(self.onChecked)
+        self.ui.radioButton_4.toggled.connect(self.onChecked)
+        self.ui.radioButton_5.toggled.connect(self.onChecked)
+        self.ui.radioButton_6.toggled.connect(self.onChecked)
+        self.ui.radioButton_7.toggled.connect(self.onChecked)
+        self.ui.radioButton_8.toggled.connect(self.onChecked)
+        self.ui.pushButton_2.clicked.connect(self.onClicked)
+
+    def onChecked(self):
+        rb = self.sender()
+        if rb.isChecked():
+            print(rb.text())
+
+    def onClicked(self):
+        items = self.ui.centralwidget.findChildren(QtWidgets.QRadioButton)
+        for rb in items:
+            if rb.isChecked():
+                self.ui.lblEgitim.setText(rb.text())
+
+
+app = QtWidgets.QApplication(sys.argv)
+win = myApp()
+win.show()
+sys.exit(app.exec_())
